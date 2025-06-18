71e606a0ca970b88067a3ab82fcb2830ad3e4fd9
Isak [Sommarjobb] <spelutvecklingeriejobb01@gmail.com>
2025-06-18 11:40:13 +0200

    fixed coin animation

--- Assets/Scripts/Money/CoinPickup.cs
+++ Assets/Scripts/Money/CoinPickup.cs
@@ -3,18 +3,30 @@ using UnityEngine;
 public class CoinPickup : MonoBehaviour
 {
     public int coinValue = 1;
+    
+    private bool _collected =false;
+    private Animator _animator;
+
+    private void Awake()
+    {
+        _animator = GetComponent<Animator>();
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player"))
         {
+            if (_collected == true)
+                return;
+            _collected = true;
+
+            _animator.SetTrigger("Collected");
+
             MoneyHolder moneyHolder = FindObjectOfType<MoneyHolder>();
             if (moneyHolder != null)
             {
                 moneyHolder.AddMoney(coinValue);
             }
-
-            Destroy(gameObject); // Coin försvinner
         }
     }
 }
