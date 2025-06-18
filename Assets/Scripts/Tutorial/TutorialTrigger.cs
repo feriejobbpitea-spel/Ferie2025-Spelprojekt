9ecdcacc66e0400faa11149a53eb243fe3a8c08d
Dator3 <spelutvecklingferiejobb01@gmail.com>
2025-06-18 15:21:46 +0200

    tutorial prompts

---
+++ Assets/Scripts/Tutorial/TutorialTrigger.cs
@@ -0,0 +1,18 @@
+using UnityEngine;
+
+public class TutorialTrigger : MonoBehaviour
+{
+    public string tutorialText;         // Texten som ska visas
+    public KeyCode keyToPress;          // Tangenten som ska tryckas för att skylten ska försvinna
+
+    private bool hasBeenTriggered = false;  // För att visa texten bara första gången
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player") && !hasBeenTriggered)
+        {
+            hasBeenTriggered = true; // Markera att vi redan visat denna
+            Manager.Instance.ShowTutorial(tutorialText, keyToPress);
+        }
+    }
+}
