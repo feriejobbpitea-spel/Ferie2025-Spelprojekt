53fb3fdcd4ad34abcd11caec5a9c49c939f4eaab
Isak [Sommarjobb] <spelutvecklingeriejobb01@gmail.com>
2025-06-26 17:17:09 +0200

    keybind prompts fixade

--- Assets/Scripts/Tutorial/TutorialTrigger.cs
+++ Assets/Scripts/Tutorial/TutorialTrigger.cs
@@ -2,17 +2,52 @@ using UnityEngine;
 
 public class TutorialTrigger : MonoBehaviour
 {
-    public string tutorialText;         // Texten som ska visas
-    public KeyCode keyToPress;          // Tangenten som ska tryckas för att skylten ska försvinna
+    public string actionName;     // Ex: "Jump", "Left", "Sprint"
+    public string tutorialMessageTemplate = "Press {0} to {1}"; // Ex: "Press A to jump"
 
-    private bool hasBeenTriggered = false;  // För att visa texten bara första gången
+    private bool hasBeenTriggered = false;
 
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.CompareTag("Player") && !hasBeenTriggered)
         {
-            hasBeenTriggered = true; // Markera att vi redan visat denna
-            PromptManager.Instance.ShowTutorial(tutorialText, keyToPress);
+            hasBeenTriggered = true;
+
+            KeyCode key = GetBoundKeyForAction(actionName);
+            string keyName = KeyCodeToString(key);
+            string message = string.Format(tutorialMessageTemplate, keyName, actionName.ToLower());
+
+            PromptManager.Instance.ShowTutorial(message, key);
         }
+    }
+
+    private KeyCode GetBoundKeyForAction(string action)
+    {
+        string keyString = PlayerPrefs.GetString("bind_" + action, GetDefaultKeyForAction(action));
+        if (System.Enum.TryParse<KeyCode>(keyString, out var key))
+        {
+            return key;
+        }
+        return KeyCode.None;
+    }
+
+    private string GetDefaultKeyForAction(string action)
+    {
+        return action switch
+        {
+            "Left" => "A",
+            "Right" => "D",
+            "Jump" => "Space",
+            "Sprint" => "LeftShift",
+            _ => "None"
+        };
+    }
+
+    private string KeyCodeToString(KeyCode key)
+    {
+        if (key == KeyCode.Space) return "SPACE";
+        if (key == KeyCode.LeftShift) return "LEFT SHIFT";
+        if (key == KeyCode.Mouse0) return "LEFT MOUSE";
+        return key.ToString().ToUpper();
     }
 }
